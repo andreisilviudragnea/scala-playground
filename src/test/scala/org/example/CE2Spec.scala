eebf6de72582a45c59ed428312c729acd2a9a1e2
Andrei Silviu Dragnea <dragnea@adobe.com>
2021-09-15 11:42:47 +0300

    Do not print

--- src/test/scala/org/example/CE2Spec.scala
+++ src/test/scala/org/example/CE2Spec.scala
@@ -5,17 +5,20 @@ import cats.effect.concurrent.Ref
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should
 
+import java.util.concurrent.atomic.AtomicReference
+
 class CE2Spec extends AnyFunSuite with should.Matchers {
   test("ce2") {
     for (_ <- 0 until 1_000_000) {
       val ref = Ref.unsafe(5)
+      val thread = new AtomicReference[Thread]()
 
-      val va = ref.get.map { v =>
-        println(Thread.currentThread().getName)
+      ref.get.map { v =>
+        thread.set(Thread.currentThread())
         v
       }.unsafeRunSync()
 
-      println(va)
+      thread.get should be(Thread.currentThread())
     }
   }
 }
