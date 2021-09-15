b7397cfd33d1d2a93937182e40701d6256ffa2fc
Andrei Silviu Dragnea <dragnea@adobe.com>
2021-09-15 08:32:49 +0300

    Repeat it 1_000_000 times

--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -4,12 +4,14 @@ import cats.effect.concurrent.Ref
 import cats.effect.IO._
 
 object Main extends App {
-  val ref = Ref.unsafe(5)
+  for (_ <- 0 until 1_000_000) {
+    val ref = Ref.unsafe(5)
 
-  val va = ref.get.map { v =>
-    println(Thread.currentThread().getName)
-    v
-  }.unsafeRunSync()
+    val va = ref.get.map { v =>
+      println(Thread.currentThread().getName)
+      v
+    }.unsafeRunSync()
 
-  println(va)
+    println(va)
+  }
 }
