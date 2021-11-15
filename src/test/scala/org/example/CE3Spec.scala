6c466c15b267fe6a9f87cbb01b42e546b1de2313
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-11-15 22:39:10 +0200

    fromCompletableFuture tests

--- src/test/scala/org/example/CE3Spec.scala
+++ src/test/scala/org/example/CE3Spec.scala
@@ -8,8 +8,10 @@ import cats.implicits.catsSyntaxParallelTraverse1
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should
 
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.atomic.AtomicReference
 import scala.concurrent.duration.DurationInt
+import scala.jdk.FutureConverters.FutureOps
 
 class CE3Spec extends AnyFunSuite with should.Matchers {
   test("ce3") {
@@ -28,5 +30,42 @@ class CE3Spec extends AnyFunSuite with should.Matchers {
 
   test("parTraverse") {
     (1 to 1_000).toList.parTraverse(v => IO.sleep(10.seconds) *> IO.pure(v)).unsafeRunSync()
+  }
+
+  test("fromCompletableFuture") {
+    val exception = new RuntimeException
+    IO
+      .fromCompletableFuture(IO(CompletableFuture.failedFuture(exception)))
+      .attempt
+      .map {
+        case Left(throwable) =>
+          throwable shouldBe exception
+          None
+        case Right(_) => Some(1)
+      }
+      .unsafeToFuture()
+      .asJava
+      .toCompletableFuture
+      .get() shouldBe None
+  }
+
+  test("fromCompletableFuture supplyAsync") {
+    val exception = new RuntimeException
+    IO
+      .fromCompletableFuture(IO(CompletableFuture.supplyAsync[Int] { () =>
+        Thread.sleep(5_000)
+        throw exception
+      }))
+      .attempt
+      .map {
+        case Left(throwable) =>
+          throwable shouldBe exception
+          None
+        case Right(_) => Some(1)
+      }
+      .unsafeToFuture()
+      .asJava
+      .toCompletableFuture
+      .get() shouldBe None
   }
 }
