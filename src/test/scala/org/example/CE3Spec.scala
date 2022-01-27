0a8816e3cd5cb36d873199dad9e750fbc7563929
Andrei Silviu Dragnea <dragnea@adobe.com>
2022-01-27 15:37:29 +0200

    Add Stream example

--- src/test/scala/org/example/CE3Spec.scala
+++ src/test/scala/org/example/CE3Spec.scala
@@ -3,8 +3,9 @@ package org.example
 import cats.effect.IO
 import cats.effect.IO._
 import cats.effect.kernel.Ref
+import cats.effect.std.Queue
 import cats.effect.unsafe.implicits.global
-import cats.implicits.catsSyntaxParallelTraverse1
+import cats.implicits._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should
 
@@ -19,17 +20,21 @@ class CE3Spec extends AnyFunSuite with should.Matchers {
       val ref = Ref.unsafe(5)
       val thread = new AtomicReference[Thread]()
 
-      ref.get.map { v =>
-        thread.set(Thread.currentThread())
-        v
-      }.unsafeRunSync()
+      ref.get
+        .map { v =>
+          thread.set(Thread.currentThread())
+          v
+        }
+        .unsafeRunSync()
 
       thread.get should not be Thread.currentThread()
     }
   }
 
   test("parTraverse") {
-    (1 to 1_000).toList.parTraverse(v => IO.sleep(10.seconds) *> IO.pure(v)).unsafeRunSync()
+    (1 to 1_000).toList
+      .parTraverse(v => IO.sleep(10.seconds) *> IO.pure(v))
+      .unsafeRunSync()
   }
 
   test("fromCompletableFuture") {
@@ -69,5 +74,24 @@ class CE3Spec extends AnyFunSuite with should.Matchers {
         .toCompletableFuture
         .get() shouldBe Some(1)
     }
+  }
+
+  test("Stream") {
+    Queue
+      .unbounded[IO, Option[Int]]
+      .flatMap { queue =>
+        for {
+          _ <- queue.offer(1.some)
+          _ <- queue.offer(2.some)
+          _ <- queue.offer(None)
+        } yield fs2.Stream.fromQueueNoneTerminated(queue)
+      }
+      .flatMap { stream =>
+        stream
+          .evalTap { v => println(v) >> IO.pure(v) }
+          .compile
+          .drain
+      }
+      .unsafeRunSync()
   }
 }
